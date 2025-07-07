e89b35fca0d3732885d39a88727f574d0241a4aa
RohitL097 <42035375+RohitL097@users.noreply.github.com>
2025-07-07 23:30:32 +0530

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,53 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 20,
+   "id": "98dabb1d-9296-46a0-9927-f1cc900a640b",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import streamlit as st"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 21,
+   "id": "b182520b-17d7-4c78-bf6e-869278a0aa39",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "st.write('hi my name is rohit')"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "2cadb7da-61b3-4383-9e09-c0b3832d2207",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
